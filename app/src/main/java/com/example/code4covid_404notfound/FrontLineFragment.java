05f8faeba61eda5b0582a306fac0819d6d31a8e0
AK <ktmabhi007@gmail.com>
2021-06-26 18:36:26 +0530

    Update

--- app/src/main/java/com/example/code4covid_404notfound/FrontLineFragment.java
+++ app/src/main/java/com/example/code4covid_404notfound/FrontLineFragment.java
@@ -2,63 +2,82 @@ package com.example.code4covid_404notfound;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FrontLineFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FrontLineFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FrontLineFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FrontLineFragment newInstance(String param1, String param2) {
-        FrontLineFragment fragment = new FrontLineFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    CardView cardDoc;
+    CardView cardMed;
+    CardView cardVol;
+    CardView cardOxy;
+    CardView cardCare;
+    CardView cardOthers;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+//        return inflater.inflate(R.layout.fragment_frontline, container, false);
+//        setContentView(R.layout.activity_main);
+        View v = inflater.inflate(R.layout.fragment_frontline,container,false);
+        cardDoc = v.findViewById(R.id.doctor);
+        cardMed = v.findViewById(R.id.medical);
+        cardVol = v.findViewById(R.id.voluenteer);
+        cardOxy = v.findViewById(R.id.oxygen);
+        cardCare = v.findViewById(R.id.caretaker);
+        cardOthers = v.findViewById(R.id.others);
+
+        cardDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("doc clicked");
+            }
+        });
+        cardMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(" cardMed clicked");
+            }
+        });
+        cardVol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("vol clicked");
+            }
+        });
+        cardOxy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("oxy clicked");
+            }
+        });
+
+        cardCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("care clicked");
+            }
+        });
+
+        cardOthers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("others clicked");
+            }
+        });
+
+    return v;
+    }
+
+
+    private void showToast(String message){
+        Toast.makeText(getActivity(),message,Toast.LENGTH_SHORT).show();
     }
 }
