3be52e120f12c49aeb36730f90e87d206be6c81b
AK <ktmabhi007@gmail.com>
2021-06-27 10:52:04 +0530

    update

--- app/src/main/java/com/example/code4covid_404notfound/NewsFragment.java
+++ app/src/main/java/com/example/code4covid_404notfound/NewsFragment.java
@@ -64,7 +64,6 @@ public class NewsFragment extends Fragment implements NewsItemClicked{
 
     private void openTipDialog() {
 
-
     }
 
 
--- app/src/main/java/com/example/code4covid_404notfound/QuizeFragment.java
+++ app/src/main/java/com/example/code4covid_404notfound/QuizeFragment.java
@@ -1,5 +1,6 @@
 package com.example.code4covid_404notfound;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +13,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -32,6 +36,13 @@ public class QuizeFragment extends Fragment {
     private ArrayList<QuizModel> quizModelArrayList;
     Random random;
     int currentScore = 0,questionAttempted = 1,currentPos;
+
+    private Button clickg;
+    private PieChart chart;
+//    private int i1 = 15;
+    public int i2 ;
+//    private int i3 = 35;
+    public int i4 ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -109,11 +120,27 @@ public class QuizeFragment extends Fragment {
         View bottomSheetView;
         bottomSheetView = LayoutInflater.from(getContext()).inflate(R.layout.score_bottom_sheet,linearLayout.findViewById(R.id.idLLScore));
         TextView scoreTV = bottomSheetView.findViewById(R.id.idTVScore);
+        chart =bottomSheetView.findViewById(R.id.pie_chart);
+        clickg = bottomSheetView.findViewById(R.id.btn_click);
         Button restartQuizeBtn = bottomSheetView.findViewById(R.id.idBtnRestart);
-        scoreTV.setText("Your Score Is \n"+currentScore+"/6");
+        scoreTV.setText("Basic Health Status Is \n"+currentScore+"/10");
+        TextView statusTV =  bottomSheetView.findViewById(R.id.idTVStatus);
+        if(currentScore>8){
+            statusTV.setText("Your Are Healthy.");
+        }
+        else if(currentScore>5){
+            statusTV.setText("Take home precaution");
+        }
+        else{
+            statusTV.setText("Try visiting the doctor");
+        }
+        i2=currentScore*10;
+        i4=(10-currentScore)*10;
+        addToPieChart();
         restartQuizeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 questionAttempted = 0;
                 currentPos = random.nextInt(quizModelArrayList.size());
                 setDataToView(currentPos);
@@ -127,8 +154,9 @@ public class QuizeFragment extends Fragment {
     }
 
     private void setDataToView(int currentPos) {
-        questionNumberTV.setText("Questions Attempted: " + questionAttempted + "/6");
-        if (questionAttempted == 6) {
+        questionNumberTV.setText("Questions Attempted: " + questionAttempted + "/10");
+        if (questionAttempted == 10) {
+
             showBottomSheet();
         } else {
             questionTV.setText(quizModelArrayList.get(currentPos).getQuestion());
@@ -146,6 +174,21 @@ public class QuizeFragment extends Fragment {
         quizModelArrayList.add(new QuizModel("Do you Have Fever Today?","Yes","No","Mild","severe","No"));
         quizModelArrayList.add(new QuizModel("Do you Have Body Pains Today?","Yes","No","Mild","severe","No"));
         quizModelArrayList.add(new QuizModel("Do you Have Breathing problem Today?","Yes","No","Mild","severe","No"));
+        quizModelArrayList.add(new QuizModel("Are you habituated to drugs and alcohol?","Yes to both","Only to drugs","Only to alcohol","I am not habituated to either","I am not habituated to either"));
+        quizModelArrayList.add(new QuizModel("Overall, how do you rate the local hospitals in your area?","Excellent","Above average","Below average","Very poor","Excellent"));
+        quizModelArrayList.add(new QuizModel("how did you take your medicine?","In the right dosage and right time","someone prepares the medicine or reminds you to take it","Miss Often","Completely unable to take it","In the right dosage and right time"));
+        quizModelArrayList.add(new QuizModel("Do you WorkOut Regularly","Yes","Sometimes","Rarely","No","Yes"));
 
+    }
+
+    private void addToPieChart() {
+        // add to pie chart
+
+//        chart.addPieSlice(new PieModel("Integer 1", i1, Color.parseColor("#FFA726")));
+        chart.addPieSlice(new PieModel("Integer 2", i2, Color.parseColor("#66BB6A")));
+//        chart.addPieSlice(new PieModel("Integer 3", i3, Color.parseColor("#EF5350")));
+        chart.addPieSlice(new PieModel("Integer 4", i4, Color.parseColor("#ff0000")));
+        chart.startAnimation();
+        clickg.setClickable(false);
     }
 }
