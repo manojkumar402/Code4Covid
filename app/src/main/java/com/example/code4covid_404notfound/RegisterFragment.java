9f34f48d3a5cc83bda3bda87949220cba7aa5763
Manoj <manojkumark2705@gmail.com>
2021-06-29 13:31:56 +0530

    ui changes

--- app/src/main/java/com/example/code4covid_404notfound/RegisterFragment.java
+++ app/src/main/java/com/example/code4covid_404notfound/RegisterFragment.java
@@ -1,8 +1,10 @@
 package com.example.code4covid_404notfound;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
@@ -14,13 +16,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -31,6 +41,10 @@ import java.util.Map;
 public class RegisterFragment extends Fragment {
 
     FirebaseAuth fAuth;
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private String TAG = "Code4Covid";
+    private int RC_SIGN_IN = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -47,6 +61,19 @@ public class RegisterFragment extends Fragment {
 //                    .replace(R.id.fragment_container,fragment).commit();
 //            finish();
 //        }
+        
+        signInButton = v.findViewById(R.id.GSignBtn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(),gso);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signin();
+            }
+        });
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,4 +107,58 @@ public class RegisterFragment extends Fragment {
         });
         return v;
     }
+
+    private void signin(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+//        NewsFragment fragment = new NewsFragment();
+//        getActivity().getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.fragment_container,fragment).commit();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try{
+            GoogleSignInAccount acc = task.getResult(ApiException.class);
+            Toast.makeText(getActivity(), "Signed In Successfully", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(acc);
+
+        }catch (ApiException e){
+            Toast.makeText(getActivity(), "Error "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(null);
+        }
+
+    }
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acc) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acc.getIdToken(),null);
+        fAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Signed In Successfully", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = fAuth.getCurrentUser();
+                    updateUI();
+                }else {
+                    Toast.makeText(getActivity(), "Error ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void updateUI() {
+
+        NewsFragment fragment = new NewsFragment();
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment)
+                .commit();
+    }
+
 }
